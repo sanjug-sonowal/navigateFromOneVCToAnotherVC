c72e9a3b157a4c3335b88222c82e94c2d772a412
sanjug-sonowal <sanjugsonowalofficial@gmail.com>
2022-12-08 11:28:52 +0530

    Initial Commit

---
+++ navigateFromOneVCToAnotherVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navigateFromOneVCToAnotherVC
+//
+//  Created by Sanjug Sonowal on 08/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
